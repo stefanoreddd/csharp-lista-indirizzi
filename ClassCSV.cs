39e8eaf29c5ee9c2bfa9d7bae62aa70eba96cae1
stefanoreddd <stefanogg95@gmail.com>
2023-04-26 17:32:09 +0200

    Aggiungere i file di progetto.

---
+++ ClassCSV.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+
+    //CLASSE
+    internal class Address
+    {
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private int zip;
+
+
+
+        //COSTRUTTORE
+        public Address(string name, string surname, string street, string city, string province, int zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip; 
+        } 
+
+
+        //GETTERS
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetSurname()
+        {
+            return this.surname;
+        }
+
+        public string GetStreet()
+        {
+            return this.street;
+        }
+
+        public string GetCity()
+        {
+            return this.city;
+        }
+
+        public string GetProvince()
+        {
+            return this.province;
+        }
+
+        public int GetZip()
+        {
+            return this.zip;
+        }
+
+
+        //SETTERS
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public void SetStreet(string street)
+        {
+            this.street = street;
+        }
+
+        public void SetCity(string city)
+        {
+            this.city = city;
+        }
+
+        public void SetProvince(string province)
+        {
+            this.province = province;
+        }
+
+        public void SetZip(int zip)
+        {
+            this.zip = zip;
+        }
+
+
+        //METODI
+
+        public override string ToString()
+        {
+            string addressList =
+            "------ INDIRIZZO ------ \n" +
+            $"Nome: {this.name} \n" +
+            $"Cognome: {this.surname} \n" +
+            $"Via: {this.street} \n" +
+            $"Città: {this.city} \n" +
+            $"Provincia: {this.province} \n" +
+            $"Zip: {this.zip} \n";
+
+            return addressList;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using csharp_lista_indirizzi;
+using System.Data;
+using System.IO;
+using System.Xml.Linq;
+
+List<Address> listaInformazioniIndirizzi = new List<Address>();
+
+try
+{
+    StreamReader file = File.OpenText("C:/Users/Red/Desktop/.NET/C#/csharp-lista-indirizzi/csharp-lista-indirizzi/addresses.csv");
+    int righeLette = 0;
+
+    while (!file.EndOfStream)
+    {
+        string rigaSingola = file.ReadLine();
+        righeLette++;
+
+        if (righeLette > 1)
+        {
+            string[] singleInfo = rigaSingola.Split(',');
+
+            if (singleInfo.Length != 6)
+            {
+                Console.WriteLine("La riga " + righeLette + " è errata");
+            }
+            else
+            {
+                int zipAddress = int.Parse(singleInfo[5]);
+
+                try
+                {
+                    Address addressInfos = new Address(singleInfo[0], singleInfo[1], singleInfo[2], singleInfo[3], singleInfo[4], zipAddress);
+                    listaInformazioniIndirizzi.Add(addressInfos);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Errore alla riga " + righeLette);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine("Errore:");
+    Console.WriteLine(e.Message);
+}
+
+foreach (Address address in listaInformazioniIndirizzi)
+{
+    Console.WriteLine(address);
+}
